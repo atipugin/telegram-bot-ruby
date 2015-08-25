2e9375b5f067bd2c3b94f72e1c67d3982b5d00a6
Sven Strothoff <sven.strothoff@gmail.com>
2015-08-25 12:03:37 +0200

    Add fields performer and title to Audio

--- lib/telegram/bot/types/audio.rb
+++ lib/telegram/bot/types/audio.rb
@@ -4,6 +4,8 @@ module Telegram
       class Audio < Base
         attribute :file_id, String
         attribute :duration, Integer
+        attribute :performer, String
+        attribute :title, String
         attribute :mime_type, String
         attribute :file_size, Integer
       end
