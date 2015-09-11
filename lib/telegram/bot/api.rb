8d0ee1fc41e49e473c93fdeac6a3a6f3f04031ee
mendable <yanberdint@gmail.com>
2015-09-11 21:20:03 +0300

    use snake_case for api methods

--- lib/telegram/bot/api.rb
+++ lib/telegram/bot/api.rb
@@ -25,7 +25,10 @@ module Telegram
       end
 
       def method_missing(method_name, *args, &block)
-        ENDPOINTS.include?(method_name.to_s) ? call(method_name, *args) : super
+        method_string = method_name.to_s
+        method_name = camelize(method_string) if method_string.include?('_')
+
+        ENDPOINTS.include?(method_string) ? call(method_name, *args) : super
       end
 
       def call(endpoint, raw_params = {})
@@ -54,6 +57,12 @@ module Telegram
       def jsonify_reply_markup(value)
         return value unless REPLY_MARKUP_TYPES.include?(value.class)
         value.to_h.to_json
+      end
+
+      def camelize(method_name)
+        words = method_name.split('_')
+        words.drop(1).each { |word| word.capitalize! }
+        words.join.to_sym
       end
     end
   end
