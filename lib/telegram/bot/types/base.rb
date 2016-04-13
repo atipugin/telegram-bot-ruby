8daa7441ff3c00b144f4bcb90a4ed00f99f40533
Alexander Tipugin <atipugin@gmail.com>
2016-04-14 01:02:18 +0300

    Improve #to_compact_hash

--- lib/telegram/bot/types/base.rb
+++ lib/telegram/bot/types/base.rb
@@ -5,7 +5,16 @@ module Telegram
         include Virtus.model
 
         def to_compact_hash
-          attributes.dup.delete_if { |_, v| v.nil? }
+          Hash[attributes.dup.delete_if { |_, v| v.nil? }.map do |key, value|
+            value =
+              if value.class.ancestors.include?(Telegram::Bot::Types::Base)
+                value.to_compact_hash
+              else
+                value
+              end
+
+            [key, value]
+          end]
         end
       end
     end
