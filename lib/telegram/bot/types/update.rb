0d62d5147bcdff61ffb1116b519fe4d2767e4a9a
Aleksey Ivanov <ialexxei@gmail.com>
2016-11-24 10:21:37 +0300

    Add `channel_post` and `edited_channel_post` to Types::Update

--- lib/telegram/bot/types/update.rb
+++ lib/telegram/bot/types/update.rb
@@ -5,6 +5,8 @@ module Telegram
         attribute :update_id, Integer
         attribute :message, Message
         attribute :edited_message, Message
+        attribute :channel_post, Message
+        attribute :edited_channel_post, Message
         attribute :inline_query, InlineQuery
         attribute :chosen_inline_result, ChosenInlineResult
         attribute :callback_query, CallbackQuery
