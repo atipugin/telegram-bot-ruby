c759131787a063188d61c4dedc5cd7b65c16cbc4
Caleb Albritton <caleb@wa9ace.net>
2018-10-18 17:58:25 -0400

    removing SIG INT trap

--- lib/telegram/bot/client.rb
+++ lib/telegram/bot/client.rb
@@ -20,10 +20,7 @@ module Telegram
 
       def listen(&block)
         logger.info('Starting bot')
-        running = true
-        Signal.trap('INT') { running = false }
-        fetch_updates(&block) while running
-        exit
+        fetch_updates(&block)
       end
 
       def fetch_updates
