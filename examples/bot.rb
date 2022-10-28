81d98a56f03fc7cef38ff541da32f97da1eb19d5
Alexander Tipugin <atipugin@gmail.com>
2022-10-28 13:11:36 +0600

    Check for message type in `example/bot.rb`

--- examples/bot.rb
+++ examples/bot.rb
@@ -7,13 +7,16 @@ token = 'replace-me-with-your-real-token'
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-    case message.text
-    when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}!")
-    when '/end'
-      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}!")
-    else
-      bot.api.send_message(chat_id: message.chat.id, text: "I don't understand you :(")
+    case message
+    when Telegram::Bot::Types::Message
+      case message.text
+      when '/start'
+        bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}!")
+      when '/end'
+        bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}!")
+      else
+        bot.api.send_message(chat_id: message.chat.id, text: "I don't understand you :(")
+      end
     end
   end
 end
