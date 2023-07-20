f0aa87328b9b26945b49b38d8c5be8c7c05471a3
Alexander Popov <alex.wayfer@gmail.com>
2023-07-20 14:17:19 +0300

    Add `default` for constrained value of `MenuButton`s
    Now `one | another` works with `.new` without required `type`.

--- lib/telegram/bot/types/menu_button_commands.rb
+++ lib/telegram/bot/types/menu_button_commands.rb
@@ -4,7 +4,7 @@ module Telegram
   module Bot
     module Types
       class MenuButtonCommands < Base
-        attribute :type, Types::String.constrained(eql: 'commands')
+        attribute :type, Types::String.constrained(eql: 'commands').default('commands')
       end
     end
   end
--- lib/telegram/bot/types/menu_button_web_app.rb
+++ lib/telegram/bot/types/menu_button_web_app.rb
@@ -4,7 +4,7 @@ module Telegram
   module Bot
     module Types
       class MenuButtonWebApp < Base
-        attribute :type, Types::String.constrained(eql: 'web_app')
+        attribute :type, Types::String.constrained(eql: 'web_app').default('web_app')
         attribute :text, Types::String
         attribute :web_app, WebAppInfo
       end
