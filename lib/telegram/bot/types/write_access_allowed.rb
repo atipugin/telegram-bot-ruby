d2a2a0dcbaee26c52e7acee92c813579ef99b306
Alexander Tipugin <atipugin@gmail.com>
2023-12-03 10:36:59 +0600

    Fix specs, add missing attributes

--- lib/telegram/bot/types/write_access_allowed.rb
+++ lib/telegram/bot/types/write_access_allowed.rb
@@ -4,7 +4,9 @@ module Telegram
   module Bot
     module Types
       class WriteAccessAllowed < Base
+        attribute? :from_request, Types::Bool
         attribute? :web_app_name, Types::String
+        attribute? :from_attachment_menu, Types::Bool
       end
     end
   end
